e529d2db94ebd1da550978bd4a4c40e9ffd1f774
karnrajsinghchauhan <karnrsc4@gmail.com>
2026-02-01 17:48:56 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+    base: "/be-my-valentine/",
   server: {
     host: "::",
     port: 8080,
